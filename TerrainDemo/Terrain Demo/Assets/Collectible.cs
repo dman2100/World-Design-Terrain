ec283ec6a0db75cf20959093084490e9d4622060
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2022-04-25 17:08:28 -0500

    myworld

---
+++ TerrainDemo/Terrain Demo/Assets/Collectible.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GetCollected();
+            Destroy(gameObject);
+        }
+    }
+
+    public void GetCollected()
+    {
+
+    }
+}
